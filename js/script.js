c7747d820a460940754d83bf183a5cb44263ba80
Pushkar Srivastav <privategafbusiness@gmail.com>
2025-03-16 19:38:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+async function fetchVersion() {
+    try {
+        // JSOn file fetch karo
+        let response = await fetch("https://urnjoya.github.io/pwacloud/json/version.json");
+        // response ko json mein convert karo
+        let data = await response.json();
+
+        document.getElementById("version-info").innerHTML = `Version: ${data.version} last update: ${data.lastupdate}`;
+    }
+    catch (error) {
+        console.log("Version fetch failed:", error);
+    }
+}
+
+// Page load hone par version fetch karo
+window.onload = fetchVersion;
